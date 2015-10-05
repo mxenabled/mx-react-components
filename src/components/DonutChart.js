89cbb152782babbe3e5af60231ef45a3f3943a1f
jmo <jason.olson@mx.com>
2015-10-05 14:06:48 -0600

    Change default styling on donut chart
    New default values
    - activeOffset: 0
    - animateOnHover: false
    - arcWidth: 10
    - baseArcColor: #F5F5F5
    - dataPointRadius: 5
    - showBaseArc: true
    
    Other changes
    - Wrapped _handleMouseEnter and _handleMouseLeave in conditional to
    only fire when animateOnHover is set to true
    - Swapped position of value and text in label
    - Set fontSize for label based on chart dimensions
    - Set cursor to pointer only if animateOnHover is true

--- src/components/DonutChart.js
+++ src/components/DonutChart.js
@@ -19,6 +19,7 @@ const DonutChart = React.createClass({
     dataPointRadius: React.PropTypes.number,
     dataPoints: React.PropTypes.array,
     defaultLabelText: React.PropTypes.string,
+    defaultLabelValue: React.PropTypes.string,
     formatter: React.PropTypes.func,
     height: React.PropTypes.number,
     onClick: React.PropTypes.func,
@@ -34,14 +35,14 @@ const DonutChart = React.createClass({
   getDefaultProps () {
     return {
       activeIndex: -1,
-      activeOffset: 3,
-      animateOnHover: true,
-      arcWidth: 80,
-      baseArcColor: '#E5E5E5',
+      activeOffset: 0,
+      animateOnHover: false,
+      arcWidth: 10,
+      baseArcColor: '#F5F5F5',
       colors: d3.scale.category20().range(),
       data: [],
       dataPointColors: d3.scale.category20b().range(),
-      dataPointRadius: 40,
+      dataPointRadius: 5,
       dataPoints: [],
       defaultLabelText: 'Roll over an item for details',
       formatter (value) {
@@ -53,7 +54,7 @@ const DonutChart = React.createClass({
       onMouseLeave () {},
       opacity: 1,
       padAngle: 0.01,
-      showBaseArc: false,
+      showBaseArc: true,
       showDataLabel: true,
       width: 360
     };
@@ -80,19 +81,23 @@ const DonutChart = React.createClass({
   },
 
   _handleMouseEnter (index) {
-    this.setState({
-      activeIndex: index
-    });
+    if (this.props.animateOnHover) {
+      this.setState({
+        activeIndex: index
+      });
 
-    this.props.onMouseEnter(index);
+      this.props.onMouseEnter(index);
+    }
   },
 
   _handleMouseLeave () {
-    this.setState({
-      activeIndex: -1
-    });
+    if (this.props.animateOnHover) {
+      this.setState({
+        activeIndex: -1
+      });
 
-    this.props.onMouseLeave();
+      this.props.onMouseLeave();
+    }
   },
 
   _renderArcs () {
@@ -184,26 +189,19 @@ const DonutChart = React.createClass({
             {this.props.children}
           </div>
         );
-      } else if (this.state.activeIndex >= 0) {
+      } else {
         const activeDataSet = this.props.data[this.state.activeIndex] || {};
-        const color = this.props.colors[this.state.activeIndex];
-        const value = this.props.formatter(activeDataSet.value);
+        const color = this.state.activeIndex === -1 ? this.props.colors[0] : this.props.colors[this.state.activeIndex];
+        const text = this.state.activeIndex === -1 ? this.props.defaultLabelText : activeDataSet.name;
+        const value = this.state.activeIndex === -1 ? this.props.formatter(this.props.defaultLabelValue) : this.props.formatter(activeDataSet.value);
 
         return (
           <div onClick={this.props.onClick} style={styles.center}>
-              <div style={styles.label}>
-                {activeDataSet.name}
-              </div>
-              <div style={[styles.value, { color }]}>
-                {value}
-              </div>
-          </div>
-        );
-      } else {
-        return (
-          <div onClick={this.props.onClick} style={styles.center}>
+            <div style={[styles.value, { color }]}>
+              {value}
+            </div>
             <div style={styles.label}>
-              {this.props.defaultLabelText}
+              {text}
             </div>
           </div>
         );
@@ -213,12 +211,13 @@ const DonutChart = React.createClass({
 
   render () {
     const position = 'translate(' + this.props.width / 2 + ',' + this.props.height / 2 + ')';
+    const fontSize = Math.min(this.props.width, this.props.height) * 0.2 + 'px';
 
     return (
-      <div style={[styles.component, this.props.style, { height: this.props.height, width: this.props.width }]}>
+      <div style={[styles.component, this.props.style, { fontSize, height: this.props.height, width: this.props.width }]}>
         {this._renderDataLabel()}
         <svg height={this.props.height} width={this.props.width}>
-          <g style={styles.pointer} transform={position}>
+          <g style={{ cursor: this.props.animateOnHover ? 'pointer' : 'auto' }} transform={position}>
             {this._renderBaseArc()}
             {this._renderArcs()}
             {this._renderDataPoints()}
@@ -242,16 +241,12 @@ const styles = {
     transform: 'translate(-50%, -50%)'
   },
   label: {
-    color: '#333',
-    fontSize: '14px',
-    fontWeight: '800'
-  },
-  pointer: {
-    cursor: 'pointer'
+    color: '#999',
+    fontSize: '0.4em',
+    marginTop: '5px'
   },
   value: {
-    fontSize: '24px',
-    fontWeight: '400'
+    fontWeight: 300
   }
 };
 
