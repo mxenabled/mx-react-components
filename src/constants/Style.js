1dddb2b4eade4213879c6eb3727a8c04f28e4d09
tumen.tumurchudur <tumen.tumurchudur@mx.com>
2015-11-20 15:29:32 -0700

    changed fontfamily back.

--- src/constants/Style.js
+++ src/constants/Style.js
@@ -30,6 +30,6 @@ module.exports = {
   },
 
   FontSize: '13px',
-  FontFamily: 'ProximaNova, Helvetica, Verdana, Tahoma, Arial, san-serif',
+  FontFamily: 'Helvetica, Arial, sans-serif',
   BoxShadow: '0 30px 30px 10px rgba(0,0,0,0.1)'
 };
