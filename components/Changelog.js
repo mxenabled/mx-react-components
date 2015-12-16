39e6cf766ece3b3368fb34e8d410558e63f01497
tommy.dugger <tommy.dugger@mx.com>
2015-12-15 17:09:18 -0700

    update change log for version 1.4.9

--- components/Changelog.js
+++ components/Changelog.js
@@ -6,6 +6,11 @@ const Changelog = React.createClass({
       <div>
         <h1>Change Log</h1>
 
+        <h3>1.4.9</h3>
+        <ul>
+          <li>Modal Component: Hide tooltip icon/label if no tooltip (<a href='https://github.com/mxenabled/mx-react-components/pull/129'>#129</a>)</li>
+        </ul>
+
         <h3>1.4.8</h3>
         <ul>
           <li>Add selectedDateColor prop to DatePicker component (<a href='https://github.com/mxenabled/mx-react-components/pull/126'>#126</a>)</li>
