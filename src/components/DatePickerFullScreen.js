22e1e1513cdb5528df6d296c586aa8f1a07f93b8
jmo <jason.olson@mx.com>
2016-01-27 16:51:31 -0700

    revert DatePickerFullScreen to createClass

--- src/components/DatePickerFullScreen.js
+++ src/components/DatePickerFullScreen.js
@@ -6,17 +6,39 @@ const Icon = require('./Icon');
 
 const StyleConstants = require('../constants/Style');
 
-class DatePickerFullScreen extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      currentDate: null,
-      inputValue: this._getInputValueByDate(this.props.defaultDate),
-      isValid: true,
-      selectedDate: this.props.defaultDate,
-      showCalendar: false
+const DatePickerFullScreen = React.createClass({
+  propTypes: {
+    closeIcon: React.PropTypes.string,
+    closeOnDateSelect: React.PropTypes.bool,
+    defaultDate: React.PropTypes.number,
+    format: React.PropTypes.string,
+    inputStyle: React.PropTypes.object,
+    isFixed: React.PropTypes.bool,
+    locale: React.PropTypes.string,
+    minimumDate: React.PropTypes.number,
+    onDateSelect: React.PropTypes.func,
+    placeholderText: React.PropTypes.string,
+    placeholderTextStyle: React.PropTypes.object,
+    selectedDateWrapperStyle: React.PropTypes.object,
+    showDayBorders: React.PropTypes.bool,
+    style: React.PropTypes.object,
+    title: React.PropTypes.string,
+    useInputForSelectedDate: React.PropTypes.bool
+  },
+
+  getDefaultProps () {
+    return {
+      closeIcon: 'close',
+      closeOnDateSelect: false,
+      format: 'MMM D, YYYY',
+      isFixed: false,
+      locale: 'en',
+      onDateSelect () {},
+      showDayBorders: false,
+      title: 'Select A Date',
+      useInputForSelectedDate: true
     };
-  }
+  },
 
   componentDidMount () {
     window.onkeyup = e => {
@@ -24,7 +46,7 @@ class DatePickerFullScreen extends React.Component {
         this._handleCloseClick();
       }
     };
-  }
+  },
 
   _getInputValueByDate (date) {
     let inputValue = null;
@@ -40,19 +62,19 @@ class DatePickerFullScreen extends React.Component {
     }
 
     return inputValue;
-  }
+  },
 
   _getSelectedDate () {
     const selectedDate = this.state.selectedDate;
 
     return selectedDate && moment.unix(selectedDate).isValid() ? this.state.selectedDate : moment().unix();
-  }
+  },
 
   _handleCloseClick () {
     this.setState({
       showCalendar: false
     });
-  }
+  },
 
   _handleDateSelect (date) {
     if (this.props.closeOnDateSelect) {
@@ -66,7 +88,7 @@ class DatePickerFullScreen extends React.Component {
     });
 
     this.props.onDateSelect(date);
-  }
+  },
 
   _handleInputBlur (evt) {
     if (evt.target.value.length === 0) {
@@ -81,13 +103,13 @@ class DatePickerFullScreen extends React.Component {
         inputValue: moment.unix(this.state.selectedDate).format(this.props.format)
       });
     }
-  }
+  },
 
   _handleInputChange (evt) {
     this.setState({
       inputValue: evt.target.value
     });
-  }
+  },
 
   _handlePreviousClick () {
     const selectedDate = moment.unix(this._getSelectedDate()).locale(this.props.locale);
@@ -98,7 +120,7 @@ class DatePickerFullScreen extends React.Component {
     this.setState({
       currentDate
     });
-  }
+  },
 
   _handleNextClick () {
     const selectedDate = moment.unix(this._getSelectedDate()).locale(this.props.locale);
@@ -109,19 +131,19 @@ class DatePickerFullScreen extends React.Component {
     this.setState({
       currentDate
     });
-  }
+  },
 
   _handleScrimClick () {
     this.setState({
       showCalendar: false
     });
-  }
+  },
 
   _toggleCalendar () {
     this.setState({
       showCalendar: !this.state.showCalendar
     });
-  }
+  },
 
   _renderMonthTable (currentDate, selectedDate) {
     const days = [];
@@ -138,7 +160,7 @@ class DatePickerFullScreen extends React.Component {
       day = (
         <div
           key={startDate.month() + '-' + startDate.date()}
-          onClick={!noSelectDay ? this._handleDateSelect.bind(this, startDate.unix()) : null}
+          onClick={!noSelectDay ? this._handleDateSelect.bind(null, startDate.unix()) : null}
           style={[
             styles.calendarDay,
             (!noSelectDay && isCurrentMonth) && styles.currentMonth
@@ -161,7 +183,7 @@ class DatePickerFullScreen extends React.Component {
     }
 
     return days;
-  }
+  },
 
   _renderSelectedDate () {
     if (this.props.useInputForSelectedDate) {
@@ -171,9 +193,9 @@ class DatePickerFullScreen extends React.Component {
         <div>
           <input
             key='input'
-            onBlur={this._handleInputBlur.bind(this)}
-            onChange={this._handleInputChange.bind(this)}
-            onClick={this._toggleCalendar.bind(this)}
+            onBlur={this._handleInputBlur}
+            onChange={this._handleInputChange}
+            onClick={this._toggleCalendar}
             style={[styles.input, this.props.inputStyle, hidePlaceholder && { backgroundColor: StyleConstants.Colors.WHITE }]}
             type='text'
             value={this.state.inputValue}
@@ -187,14 +209,14 @@ class DatePickerFullScreen extends React.Component {
       return (
         <div
           key='selectedDate'
-          onClick={this._toggleCalendar.bind(this)}
+          onClick={this._toggleCalendar}
           style={styles.selectedDate}
         >
           {this.state.inputValue}
         </div>
       );
     }
-  }
+  },
 
   _renderTitle (styles) {
     if (this.props.title) {
@@ -204,7 +226,7 @@ class DatePickerFullScreen extends React.Component {
         </div>
       );
     }
-  }
+  },
 
   render () {
     const selectedDate = moment.unix(this._getSelectedDate()).locale(this.props.locale);
@@ -227,7 +249,7 @@ class DatePickerFullScreen extends React.Component {
           this.state.showCalendar && styles.calendarShow,
           this.props.isFixed && { position: 'fixed' }
         ]}>
-          <div onClick={this._handleCloseClick.bind(this)} style={styles.close}>
+          <div onClick={this._handleCloseClick} style={styles.close}>
             <Icon
               size={20}
               style={styles.closeIcon}
@@ -239,14 +261,14 @@ class DatePickerFullScreen extends React.Component {
             {this._renderTitle(styles)}
             <div className='mx-date-picker-full-screen-calendar-header' key='calendarHeader' style={[styles.calendarHeader, { borderBottomStyle: this.props.showDayBorders ? 'solid' : 'none' }, styles.clearFix]}>
               <Icon
-                onClick={this._handlePreviousClick.bind(this)}
+                onClick={this._handlePreviousClick}
                 size='32px'
                 style={[styles.navIcon, styles.navLeft, this.props.showDayBorders && styles.borderRight]}
                 type='caret-left'
               />
               {currentDate.format('MMMM YYYY')}
               <Icon
-                onClick={this._handleNextClick.bind(this)}
+                onClick={this._handleNextClick}
                 size='32px'
                 style={[styles.navIcon, styles.navRight, this.props.showDayBorders && styles.borderLeft]}
                 type='caret-right'
@@ -261,38 +283,7 @@ class DatePickerFullScreen extends React.Component {
       </div>
     );
   }
-}
-
-DatePickerFullScreen.propTypes = {
-  closeIcon: React.PropTypes.string,
-  closeOnDateSelect: React.PropTypes.bool,
-  defaultDate: React.PropTypes.number,
-  format: React.PropTypes.string,
-  inputStyle: React.PropTypes.object,
-  isFixed: React.PropTypes.bool,
-  locale: React.PropTypes.string,
-  minimumDate: React.PropTypes.number,
-  onDateSelect: React.PropTypes.func,
-  placeholderText: React.PropTypes.string,
-  placeholderTextStyle: React.PropTypes.object,
-  selectedDateWrapperStyle: React.PropTypes.object,
-  showDayBorders: React.PropTypes.bool,
-  style: React.PropTypes.object,
-  title: React.PropTypes.string,
-  useInputForSelectedDate: React.PropTypes.bool
-};
-
-DatePickerFullScreen.defaultProps = {
-  closeIcon: 'close',
-  closeOnDateSelect: false,
-  format: 'MMM D, YYYY',
-  isFixed: false,
-  locale: 'en',
-  onDateSelect () {},
-  showDayBorders: false,
-  title: 'Select A Date',
-  useInputForSelectedDate: true
-};
+});
 
 const styles = {
   calendarDay: {
