eeea2fd675655d291888f8f4460116e827d7e28c
Luke Schunk <lukeschunk@gmail.com>
2016-02-01 02:27:20 -0700

    Updated for Travis Errors

--- src/components/Input.js
+++ src/components/Input.js
@@ -35,13 +35,13 @@ const styles = {
     borderRadius: '3px',
     width: '100%',
 
-	  ':focus': {
-		  outline: 'focus-ring-color auto 4px',
-		  backgroundColor: 'white'
-	  },
-	  ':hover': {
-		  backgroundColor: 'rgb(249, 249, 249)'
-	  }
+    ':focus': {
+      outline: 'focus-ring-color auto 4px',
+      backgroundColor: 'white'
+    },
+    ':hover': {
+      backgroundColor: 'rgb(249, 249, 249)'
+    }
   },
   label: {
     display: 'block',
@@ -69,65 +69,65 @@ const Input = React.createClass({
     text: React.PropTypes.bool
   },
 
-  getDefaultProps() {
-	  return{
+  getDefaultProps () {
+    return {
       currency: false,
-		  email: false,
-      label: "",
-		  phone: false,
-      placeholderText: "Placeholder Text...",
-		  prefix: "",
+      email: false,
+      label: '',
+      phone: false,
+      placeholderText: 'Placeholder Text...',
+      prefix: '',
       text: false,
-	  };
+    };
   },
 
   getInitialState () {
     return {
       valid: false
-    }
+    };
   },
 
   _getValidIcon () {
     //Returns the 'check' type icon if the input is valid
-    let isValid = this.state.valid ? 'check' : '';
+    const isValid = this.state.valid ? 'check' : '';
+    
     return isValid;
   },
 
-  _handleChange(event) {
-	  const textValue = event.target.value;
+  _handleChange (event) {
+    const textValue = event.target.value;
 
     //onChange Handler - lets parent know about change
-	  this.props.handleInputValueChange(textValue);
-
-	  this._handleValidate(textValue);
+    this.props.handleInputValueChange(textValue);
+    this._handleValidate(textValue);
   },
 
-  _handleValidate(textValue) {
-	  let isValid = this._validateText(textValue);
-	    this.setState({
-		    valid: isValid
-	    });
+  _handleValidate (textValue) {
+    const isValid = this._validateText(textValue);
+    this.setState({
+      valid: isValid
+    });
   },
 
-  _validateText(inputTextValue) {
-	  //Validates for Email (version 1)
-	  if(this.props.email) {
-        return /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/.test( inputTextValue );
-	  }
-	  //Validates for Phone
-	  if(this.props.phone) {
-		  return /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/.test( inputTextValue );
-	  }
-	  //Validates for currency
-	  if(this.props.currency) {
-		  return /^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$/.test( inputTextValue );
-	  }
+  _validateText (inputTextValue) {
+    //Validates for Email (version 1)
+    if (this.props.email) {
+      return (/^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/).test( inputTextValue );
+    }
+    //Validates for Phone
+    if (this.props.phone) {
+      return (/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/).test( inputTextValue );
+    }
+    //Validates for currency
+    if (this.props.currency) {
+      return /^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$/.test( inputTextValue );
+    }
     //Allows any characters; standard input
-	  if(this.props.text) {
-	    return true;
-  	}
+    if (this.props.text) {
+      return true;
+    }
     //Checks Against Custom Value
-    if(this.props.custom === inputTextValue) {
+    if (this.props.custom === inputTextValue) {
       return true;
     }
   },
@@ -141,20 +141,18 @@ const Input = React.createClass({
         <div style={styles.inputHolder} >
 
           <span style={styles.prefix}> {this.props.prefix} </span>
-			    <input
+          <input
             onChange={this._handleChange}
             placeholder={this.props.placeholderText}
             style={styles.input}
-            type="text"
-            value={this.props.defaultValueProp}
-			    />
+            type='text'
+          />
           <Icon
             size={20}
-				    type={this._getValidIcon()}
-				    style={styles.icon}
-			    />
-		    </div>
-
+            type={this._getValidIcon()}
+            style={styles.icon}
+          />
+        </div>
       </div>
     );
   }
