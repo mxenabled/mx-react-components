99741be2b37d609bdae2da113615a4f0150dfff1
tommy.dugger <tommy.dugger@mx.com>
2016-03-07 17:14:15 -0700

    update TimeBasedLineChart docs

--- components/TimeBasedLineChartDocs.js
+++ components/TimeBasedLineChartDocs.js
@@ -34,22 +34,17 @@ const TimeBasedLineChartDocs = React.createClass({
         <TimeBasedLineChart
           breakPointDate={moment().startOf('month').unix()}
           breakPointLabel={'This Month'}
-          dashedFutureLine={false}
           data={lineChartData}
           height={400}
           margin={{ top: 30, right: 0, bottom: 30, left: 75 }}
           rangeType={'month'}
-          shadeAreaBelowZero={true}
-          showZeroYTick={true}
+          shadeBelowZero={true}
+          showZeroLine={true}
           style={{ boxSizing: 'content-box' }}
           width={700}
         />
 
         <h3>Usage</h3>
-        <h5>areaBelowZeroColor <label>String</label></h5>
-        <p>Default: rgb(238, 66, 53)</p>
-        <p>A CSS color value that sets the color of the area below zero.</p>
-
         <h5>breakPointDate <label>Number</label></h5>
         <p>Default: today (as a Unix timestamp)</p>
         <p>A Unix timestamp that sets the breakpoint for the chart. The breakpoint is displayed as a vertical line on the chart. If 'dashedFutureLine' is set to 'true', then the line after the breakpoint will be dashed instead of solid.</p>
@@ -60,10 +55,6 @@ const TimeBasedLineChartDocs = React.createClass({
 
         <h5>children <label>Node(s)</label></h5>
         <p>If defined, these nodes will be used for the tooltip. Typically this is used in conjunction with 'hoverCallBack' to create custom tooltips.</p>
-
-        <h5>dashedFutureLine <label>Boolean</label></h5>
-        <p>Default: true</p>
-        <p>If set to 'true', then the line after the breakpoint will be dotted instead of solid.</p>
 
         <h5>data <label>Arrray</label></h5>
         <p>An array of data objects that are used to set the x axis, y axis, and line values. Example:</p>
@@ -80,16 +71,9 @@ const TimeBasedLineChartDocs = React.createClass({
         <p>Default: 400</p>
         <p>Sets the overall height of the chart.</p>
 
-        <h5>hoverCallBack <label>Function</label></h5>
-        <p>A method to be called when hovering over a data point on the chart. This is where you can </p>
-
         <h5>lineColor <label>String</label></h5>
         <p>Default: #359BCF</p>
         <p>A CSS color value that sets the color of the data line.</p>
-
-        <h5>defaultLabelText <label>String</label></h5>
-        <p>Default: Roll over item for details</p>
-        <p>The text to display in the data label when not hovering over a slice.</p>
 
         <h5>margin <label>Object</label></h5>
         <p>A object that contains top, right, bottom, and left margin values. These can be used to adjust the spacing around the chart. Default:</p>
@@ -108,25 +92,21 @@ const TimeBasedLineChartDocs = React.createClass({
         <p>Default: 'day'</p>
         <p>This should match the type of data you're passing in. It is used to normalize the data points to month or day intervals.</p>
 
-        <h5>shadeAreaBelowZero <label>Boolean</label></h5>
+        <h5>shadeBelowZero <label>Boolean</label></h5>
         <p>Default: false</p>
         <p>If set to 'true', then the area below zero will be shaded. This is set to 'true' in the example above.</p>
+
+        <h5>shadeFutureOnGraph <label>Boolean</label></h5>
+        <p>Default: true</p>
+        <p>Set this to 'false' if don't want any part of the graph that is in the future to be shaded.</p>
 
         <h5>showBreakPoint <label>Boolean</label></h5>
         <p>Default: true</p>
         <p>Set this to 'false' if you don't want to display the vertical line and label that indicates the breakpoint.</p>
 
-        <h5>showTooltips <label>Boolean</label></h5>
-        <p>Default: true</p>
-        <p>Set this to 'false' if you don't want to display a tooltip when hovering over the data points.</p>
-
-        <h5>showZeroYTick <label>Boolean</label></h5>
+        <h5>showZeroLine <label>Boolean</label></h5>
         <p>Default: false</p>
         <p>If set to 'true', then a tick, label, and line will be displayed for the '0' yAxis value.</p>
-
-        <h5>staticXAxis <label>Boolean</label></h5>
-        <p>Default: true</p>
-        <p>If set to false, the x axis labels will only be shown when hovering over a data point and a static x axis label will be displayed under the breakpoint.</p>
 
         <h5>width <label>Number</label></h5>
         <p>Default: 550</p>
@@ -134,6 +114,9 @@ const TimeBasedLineChartDocs = React.createClass({
 
         <h5>yAxisFormatter <label>Function</label></h5>
         <p>This function will be used to determine the format of the y axis tick labels. If should return some type of value.</p>
+
+        <h5>zeroState <label>Node</label></h5>
+        <p>A JSX node rendered if the data supplied has no length.</p>
 
         <h3>Example</h3>
         <Markdown lang='js'>
@@ -157,13 +140,12 @@ const TimeBasedLineChartDocs = React.createClass({
     <TimeBasedLineChart
       breakPointDate={moment().startOf('month').unix()}
       breakPointLabel={'This Month'}
-      dashedFutureLine={false}
       data={lineChartData}
       height={400}
       margin={{ top: 30, right: 0, bottom: 30, left: 75 }}
       rangeType={'month'}
-      shadeAreaBelowZero={true}
-      showZeroYTick={true}
+      shadeBelowZero={true}
+      showZeroLine={true}
       style={{ boxSizing: 'content-box' }}
       width={700}
     />
