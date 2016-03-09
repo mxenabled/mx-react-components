9063a2314aa02e64c262215a38d0ccaa518f7e2a
Ryan Moore <ryan.moore@mx.com>
2016-03-09 11:45:53 -0700

    Easing in animation slide open and close, header styles and icon with click handler

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,9 +1,14 @@
 const React = require('react');
 const Velocity = require('velocity-animate');
 
+const StyleConstants = require('../constants/Style');
+
+const Icon = require('../components/Icon');
+
 const Drawer = React.createClass({
   propTypes: {
     duration: React.PropTypes.number,
+    easing: React.PropTypes.array,
     isOpen: React.PropTypes.bool
   },
 
@@ -11,7 +16,7 @@ const Drawer = React.createClass({
     return {
       duration: 500,
       isOpen: false,
-      easing: [.28, .14, .34, 1.04]
+      easing: [0.28, 0.14, 0.34, 1.04]
     };
   },
 
@@ -23,6 +28,10 @@ const Drawer = React.createClass({
     if (newProps.isOpen !== this.props.isOpen) {
       this._renderTransition(newProps.isOpen);
     }
+  },
+
+  _handleArrowClick () {
+    this._renderTransition(true);
   },
 
   _renderTransition (isOpen) {
@@ -38,8 +47,10 @@ const Drawer = React.createClass({
 
   render () {
     return (
-      <div ref='component' style={styles}>
-        HELLO DRAWER
+      <div ref='component' style={styles.component}>
+        <nav style={styles.nav}><Icon onClick={this._handleArrowClick} size={20} style={styles.icon} type='arrow-left'/></nav>
+        <header></header>
+        <div></div>
       </div>
     );
   }
@@ -47,12 +58,24 @@ const Drawer = React.createClass({
 });
 
 const styles = {
-  top: 0,
-  bottom: 0,
-  right: -800,
-  position: 'absolute',
-  width: 800,
-  backgroundColor: '#eaeaea'
+  component: {
+    top: 0,
+    bottom: 0,
+    right: -800,
+    position: 'absolute',
+    width: 800,
+    backgroundColor: '#eaeaea'
+  },
+  icon: {
+    color: StyleConstants.Colors.ASH,
+    position: 'absolute',
+    left: 25
+  },
+  nav: {
+    padding: '15px 25px',
+    borderBottom: 'solid 1px ' + StyleConstants.Colors.ASH,
+    height: 15
+  }
 };
 
 module.exports = Drawer;
