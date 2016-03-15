92e9a9005217536ef11bf2da9dbcaec0190505a2
Ryan Moore <ryan.moore@mx.com>
2016-03-15 15:57:03 -0600

    updating mx eslint and fixing rules errors

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -37,9 +37,19 @@ const Drawer = React.createClass({
 
     return this.props.navConfig ?
       <nav ref={(ref) => (this._nav = ref)} style={styles.nav}>
-        <Icon onClick={this.props.navConfig.onPreviousClick} size={25} style={styles.icons} type='caret-left'/>
+        <Icon
+          onClick={this.props.navConfig.onPreviousClick}
+          size={25}
+          style={styles.icons}
+          type='caret-left'
+        />
         {this.props.navConfig.label}
-        <Icon onClick={this.props.navConfig.onNextClick} size={25} style={styles.icons} type='caret-right'/>
+        <Icon
+          onClick={this.props.navConfig.onNextClick}
+          size={25}
+          style={styles.icons}
+          type='caret-right'
+        />
       </nav> : null;
   },
 
@@ -91,7 +101,12 @@ const Drawer = React.createClass({
       <div ref={(ref) => (this._component = ref)} style={styles.component}>
         <header style={styles.header}>
           <span ref={(ref) => (this._backArrow = ref)} style={styles.backArrow}>
-            <Icon onClick={this._handleCloseClick} size={25} style={styles.icons}type='arrow-left'/>
+            <Icon
+              onClick={this._handleCloseClick}
+              size={25}
+              style={styles.icons}
+              type='arrow-left'
+            />
           </span>
           {this._renderNav()}
         </header>
