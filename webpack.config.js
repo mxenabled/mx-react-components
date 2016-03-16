c2a4f0a3530d30b8fffeb5403d6049cb662818ee
Ryan Moore <ryan.moore@mx.com>
2016-03-16 16:32:39 -0600

    ignoring autoit.js file in highlight.js that causes webpack to fail

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       }
-    ]
+    ],
+    noParse: [/autoit.js/]
   },
   output: {
     filename: './bundle.js'
