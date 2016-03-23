be8aa178b7b7624f3a82f6041dad98dda17834a0
Rachel Wolthuis <rachel.wolthuis@mx.com>
2016-03-23 10:37:29 -0600

    changes sync icon to spinner icon on loading buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -50,7 +50,7 @@ const Button = React.createClass({
       return (
         <div>
         <Spin>
-            <Icon size='20' style={spinnerStyles} type='sync' />
+            <Icon size='20' style={spinnerStyles} type='spinner' />
         </Spin>
           {this.props.actionText ? <div style={actionTextStyles}> {this.props.actionText} </div> : null }
         </div>
