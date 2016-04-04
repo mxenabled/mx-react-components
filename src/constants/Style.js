583a5438be306c0886711df7adb200617b594c9c
jmo <jason.olson@mx.com>
2016-04-04 09:23:46 -0600

    rename to adjustHexOpacity

--- src/constants/Style.js
+++ src/constants/Style.js
@@ -84,7 +84,7 @@ module.exports = {
   },
 
 
-  adjustOpacity (color, opacity) {
+  adjustHexOpacity (color, opacity) {
     const r = parseInt(color.slice(1, 3), 16);
     const g = parseInt(color.slice(3, 5), 16);
     const b = parseInt(color.slice(5, 7), 16);
