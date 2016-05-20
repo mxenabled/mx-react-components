fbae4fc11056403570f79d398b86f4547719b145
Rachel Wolthuis <rachel.wolthuis@mx.com>
2016-05-20 16:31:41 -0600

    updates docs

--- components/Changelog.js
+++ components/Changelog.js
@@ -6,10 +6,16 @@ const Changelog = React.createClass({
       <div>
         <h1>Change Log</h1>
 
+        <h3>3.1.29</h3>
+        <ul>
+          <li>Disabled onClick for `disabled` type button components (<a href='https://github.com/mxenabled/mx-react-components/pull/313'>#313</a>)</li>
+        </ul>
+
         <h3>3.1.28</h3>
         <ul>
           <li>Adds new icons duplicate and kabob horizontal to the Icon component (<a href='https://github.com/mxenabled/mx-react-components/pull/311'>#311</a>)</li>
         </ul>
+
         <h3>3.1.27</h3>
         <ul>
           <li>Fixed a typo in the DatePicker component (<a href='https://github.com/mxenabled/mx-react-components/pull/308'>#308</a>)</li>
