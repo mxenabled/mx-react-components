7a672b63322fb9186d6d37abbe7f475d449305a8
tumen.tumurchudur <tumen.tumurchudur@mx.com>
2016-05-31 10:46:42 -0600

    updated changelog

--- components/Changelog.js
+++ components/Changelog.js
@@ -6,6 +6,11 @@ const Changelog = React.createClass({
       <div>
         <h1>Change Log</h1>
 
+        <h3>3.1.33</h3>
+        <ul>
+          <li>Adds the ability set the color of each bar in the Barchart component (<a href='https://github.com/mxenabled/mx-react-components/pull/321'>#321</a>)</li>
+        </ul>
+
         <h3>3.1.32</h3>
         <ul>
           <li>Adds the ability set the color of a clicked bar for Barchart (<a href='https://github.com/mxenabled/mx-react-components/pull/320'>#320</a>)</li>
