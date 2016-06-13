6517bab979316307d6cdb09aa5b80c85e9bf22f6
tommy.dugger <tommy.dugger@mx.com>
2016-06-13 17:07:35 -0600

    Add new Calendar component
    ports the calendar display logic to its own component for reuse.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,15 +6,11 @@ const Icon = require('./Icon');
 
 const StyleConstants = require('../constants/Style');
 
-const DatePicker = React.createClass({
+const Calendar = React.createClass({
   propTypes: {
-    closeOnDateSelect: React.PropTypes.bool,
-    defaultDate: React.PropTypes.number,
-    format: React.PropTypes.string,
     locale: React.PropTypes.string,
     minimumDate: React.PropTypes.number,
     onDateSelect: React.PropTypes.func,
-    placeholderText: React.PropTypes.string,
     primaryColor: React.PropTypes.string,
     selectedDate: React.PropTypes.number,
     style: React.PropTypes.object
@@ -22,26 +18,16 @@ const DatePicker = React.createClass({
 
   getDefaultProps () {
     return {
-      closeOnDateSelect: false,
-      format: 'MMM D, YYYY',
       locale: 'en',
       onDateSelect () {},
-      placeholderText: 'Select A Date',
       primaryColor: StyleConstants.Colors.PRIMARY
     };
   },
 
   getInitialState () {
     return {
-      currentDate: this.props.selectedDate || this.props.defaultDate || moment().unix(),
-      showCalendar: false
+      currentDate: this.props.selectedDate || moment().unix()
     };
-  },
-
-  componentDidMount () {
-    if (this.props.defaultDate) {
-      console.warn('WARNING: defaultDate has been replaced with selectedDate and will be removed in a future release. Check usage of ' + this.constructor.displayName + '.');
-    }
   },
 
   componentWillReceiveProps (newProps) {
@@ -50,20 +36,9 @@ const DatePicker = React.createClass({
         currentDate: newProps.selectedDate
       });
     }
-
-    if (newProps.defaultDate && newProps.defaultDate !== this.props.defaultDate) {
-      console.warn('WARNING: defaultDate has been replaced with selectedDate and will be removed in a future release. Check usage of ' + this.constructor.displayName + '.');
-      this.setState({
-        currentDate: newProps.defaultDate
-      });
-    }
   },
 
   _handleDateSelect (date) {
-    if (this.props.closeOnDateSelect) {
-      this._handleScrimClick();
-    }
-
     this.props.onDateSelect(date);
   },
 
@@ -83,18 +58,6 @@ const DatePicker = React.createClass({
     });
   },
 
-  _handleScrimClick () {
-    this.setState({
-      showCalendar: false
-    });
-  },
-
-  _toggleCalendar () {
-    this.setState({
-      showCalendar: !this.state.showCalendar
-    });
-  },
-
   _renderMonthTable () {
     const styles = this.styles();
     const days = [];
@@ -103,7 +66,7 @@ const DatePicker = React.createClass({
 
     while (moment(startDate).isBefore(endDate)) {
       const isCurrentMonth = startDate.isSame(moment.unix(this.state.currentDate), 'month');
-      const isSelectedDay = startDate.isSame(moment.unix(this.props.selectedDate || this.props.defaultDate), 'day');
+      const isSelectedDay = startDate.isSame(moment.unix(this.props.selectedDate), 'day');
       const isToday = startDate.isSame(moment(), 'day');
       const disabledDay = this.props.minimumDate ? startDate.isBefore(moment.unix(this.props.minimumDate)) : null;
 
@@ -135,55 +98,35 @@ const DatePicker = React.createClass({
 
     return (
       <div style={styles.component}>
-        <div onClick={this._toggleCalendar} style={styles.selectedDateWrapper}>
+        <div style={styles.calendarHeader}>
           <Icon
+            onClick={this._handlePreviousClick}
             size={20}
-            style={styles.selectedDateIcon}
-            type='calendar'
+            style={styles.calendayHeaderNav}
+            type='caret-left'
           />
-          <div style={styles.selectedDateText}>
-            {(this.props.selectedDate || this.props.defaultDate) ? moment.unix(this.props.selectedDate || this.props.defaultDate).format(this.props.format) : this.props.placeholderText}
+          <div>
+            {moment(this.state.currentDate, 'X').format('MMMM YYYY')}
           </div>
           <Icon
+            onClick={this._handleNextClick}
             size={20}
-            style={styles.selectedDateCaret}
-            type={this.state.showCalendar ? 'caret-up' : 'caret-down'}
+            style={styles.calendayHeaderNav}
+            type='caret-right'
           />
         </div>
-        <div style={styles.calendarWrapper}>
-          <div style={styles.calendarHeader}>
-            <Icon
-              onClick={this._handlePreviousClick}
-              size={20}
-              style={styles.calendayHeaderNav}
-              type='caret-left'
-            />
-            <div>
-              {moment(this.state.currentDate, 'X').format('MMMM YYYY')}
-            </div>
-            <Icon
-              onClick={this._handleNextClick}
-              size={20}
-              style={styles.calendayHeaderNav}
-              type='caret-right'
-            />
-          </div>
-          <div style={styles.calendarWeek}>
-            {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day, i) => {
-              return (
-                <div key={i} style={styles.calendarWeekDay}>
-                  {day}
-                </div>
-              );
-            })}
-          </div>
-          <div style={styles.calendarTable}>
-            {this._renderMonthTable()}
-          </div>
+        <div style={styles.calendarWeek}>
+          {['S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day, i) => {
+            return (
+              <div key={i} style={styles.calendarWeekDay}>
+                {day}
+              </div>
+            );
+          })}
         </div>
-        {(this.state.showCalendar) ? (
-          <div onClick={this._handleScrimClick} style={styles.scrim} />
-        ) : null }
+        <div style={styles.calendarTable}>
+          {this._renderMonthTable()}
+        </div>
       </div>
     );
   },
@@ -192,55 +135,13 @@ const DatePicker = React.createClass({
     return {
       component: Object.assign({
         backgroundColor: StyleConstants.Colors.WHITE,
-        borderColor: this.state.showCalendar ? this.props.primaryColor : StyleConstants.Colors.FOG,
-        borderRadius: 3,
-        borderStyle: 'solid',
-        borderWidth: 1,
-        boxSizing: 'border-box',
-        color: StyleConstants.Colors.BLACK,
-        display: 'inline-block',
-        fontFamily: StyleConstants.FontFamily,
-        fontSize: StyleConstants.FontSizes.MEDIUM,
-        position: 'relative',
-        width: '100%'
-      }, this.props.style),
-
-      // Selected Date styles
-      selectedDateWrapper: {
-        alignItems: 'center',
-        cursor: 'pointer',
-        display: 'flex',
-        justifyContent: 'space-between',
-        padding: '10px 15px',
-        position: 'relative'
-      },
-      selectedDateIcon: {
-        fill: this.props.primaryColor,
-        marginRight: 5
-      },
-      selectedDateText: {
-        color: (this.props.selectedDate || this.props.defaultDate) ? StyleConstants.Colors.CHARCOAL : StyleConstants.Colors.ASH,
-        flex: 1
-      },
-      selectedDateCaret: {
-        fill: this.state.showCalendar ? this.props.primaryColor : StyleConstants.Colors.ASH
-      },
-
-      //Calendar Styles
-      calendarWrapper: {
-        backgroundColor: StyleConstants.Colors.WHITE,
         border: '1px solid ' + StyleConstants.Colors.FOG,
         borderRadius: 3,
-        boxShadow: StyleConstants.ShadowHigh,
         boxSizing: 'border-box',
-        display: this.state.showCalendar ? 'block' : 'none',
         marginTop: 10,
         padding: 20,
-        position: 'absolute',
-        right: 0,
-        width: 287,
-        zIndex: 10
-      },
+        width: 287
+      }, this.props.style),
 
       //Calendar Header
       calendarHeader: {
@@ -316,18 +217,9 @@ const DatePicker = React.createClass({
       selectedDay: {
         backgroundColor: this.props.primaryColor,
         color: StyleConstants.Colors.WHITE
-      },
-
-      scrim: {
-        bottom: 0,
-        left: 0,
-        position: 'fixed',
-        right: 0,
-        top: 0,
-        zIndex: 9
       }
     };
   }
 });
 
-module.exports = Radium(DatePicker);
+module.exports = Radium(Calendar);
