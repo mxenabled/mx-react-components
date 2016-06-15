90c23b03128fc5f16fd40cef10f1f93609b37c45
Ryan Moore <ryan.moore@mx.com>
2016-06-14 22:34:06 -0600

    Simple test to verify div is loaded with class

--- src/components/__tests__/loader-test.js
+++ src/components/__tests__/loader-test.js
@@ -1,5 +1,20 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactTestUtils = require('react-addons-test-utils');
+const Loader = require('../Loader');
+
 describe('demo test', () => {
   it('should run a test', () => {
     expect(true).toEqual(true);
+  });
+
+  it('renders a div with a className prop', () => {
+    const renderer = ReactTestUtils.createRenderer();
+
+    renderer.render(<Loader isLoading={true} isRelative={true} />);
+    const result = renderer.getRenderOutput();
+
+    expect(result.type).toBe('div');
+    expect(result.props.className).toBe('mx-loader');
   });
 });
