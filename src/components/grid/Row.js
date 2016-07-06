23ecfcc6ba1a7c390972c372713ccb991a1bd1fb
tommy.dugger <tommy.dugger@mx.com>
2016-07-06 15:30:25 -0600

    box sizing

--- src/components/grid/Row.js
+++ src/components/grid/Row.js
@@ -3,7 +3,7 @@ const React = require('react');
 const Row = React.createClass({
   render () {
     return (
-      <div className={'row'}>
+      <div className={'row'} style={{ boxSizing: 'border-box' }}>
         {this.props.children}
       </div>
     );
