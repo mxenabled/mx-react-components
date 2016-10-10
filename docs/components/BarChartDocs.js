6af0396c09d722bf798f580778c4ee3622e2feff
lukeschunk <luke.schunk@mx.com>
2016-10-10 13:35:31 -0600

    removed change this

--- docs/components/BarChartDocs.js
+++ docs/components/BarChartDocs.js
@@ -96,7 +96,7 @@ const BarChartDocs = React.createClass({
       <div>
         <h1>
           BarChart
-          <label>CHANGE THIS A D3 bar chart that supports labels for the bars and tooltips.</label>
+          <label>A D3 bar chart that supports labels for the bars and tooltips.</label>
         </h1>
 
         <h3 style={styles.demoHeader}>Demo</h3>
