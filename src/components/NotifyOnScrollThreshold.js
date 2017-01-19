799de403199741cde42de08b8d3d55075e651ac3
tommy.dugger <tommy.dugger@mx.com>
2017-01-18 19:02:56 -0700

    Adds new NotifyOnScrollThreshold component

---
+++ src/components/NotifyOnScrollThreshold.js
@@ -0,0 +1,48 @@
+const React = require('react');
+const _throttle = require('lodash/throttle');
+
+const NotifyOnScrollThreshold = React.createClass({
+  propTypes: {
+    children: React.PropTypes.func,
+    threshold: React.PropTypes.number
+  },
+
+  getInitialState () {
+    return {
+      thresholdMet: false
+    };
+  },
+
+  componentDidMount () {
+    this._handleScroll = _throttle(this._handleScroll, 200);
+
+    this.container.parentElement.addEventListener('scroll', this._handleScroll);
+  },
+
+  componentWillUnmount () {
+    this.container.parentElement.removeEventListener('scroll', this._handleScroll);
+  },
+
+  _handleScroll (evt) {
+    const element = evt.target;
+    const availableHeight = element.scrollHeight - element.clientHeight;
+
+    const position = element.scrollTop / availableHeight;
+
+    if (position >= this.props.threshold) {
+      this.setState({ thresholdMet: true });
+    } else if (this.state.thresholdMet && position < this.props.threshold) {
+      this.setState({ thresholdMet: false });
+    }
+  },
+
+  render () {
+    return (
+      <div ref={(ref) => this.container = ref}>
+        {this.props.children(this.state.thresholdMet)}
+      </div>
+    );
+  }
+});
+
+module.exports = NotifyOnScrollThreshold;
