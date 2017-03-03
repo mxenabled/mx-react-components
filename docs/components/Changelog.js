ea0362c21bda02bbc2ca38c01b5694207992a212
Dan Jones <dan.jones@moneydesktop.com>
2017-03-03 14:22:21 -0700

    Update change log

--- docs/components/Changelog.js
+++ docs/components/Changelog.js
@@ -6,6 +6,13 @@ const Changelog = React.createClass({
       <div>
         <h1>Change Log</h1>
 
+        <h3>Release Candidate 5.0.0-rc.59</h3>
+        <ul>
+          <li>
+            Adds Needle Icon Component (<a href='https://github.com/mxenabled/mx-react-components/pull/522'>#522</a>)
+          </li>
+        </ul>
+
         <h3>Release Candidate 5.0.0-rc.58</h3>
         <ul>
           <li>
