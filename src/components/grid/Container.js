b89bd80dee659dac709f5ea1bfaf59859121c574
Jared Perkins <jaredperkins24@gmail.com>
2017-03-15 15:40:39 -0600

    Adding a styles prop to the Container component

--- src/components/grid/Container.js
+++ src/components/grid/Container.js
@@ -2,7 +2,8 @@ const React = require('react');
 
 const Container = React.createClass({
   propTypes: {
-    fluid: React.PropTypes.bool
+    fluid: React.PropTypes.bool,
+    styles: React.PropTypes.object
   },
 
   getDefaultProps () {
@@ -13,7 +14,7 @@ const Container = React.createClass({
 
   render () {
     return (
-      <div className={'container' + (this.props.fluid ? '-fluid' : '')} style={{ boxSizing: 'border-box' }}>
+      <div className={'container' + (this.props.fluid ? '-fluid' : '')} style={Object.assign({}, this.props.styles, { boxSizing: 'border-box' })}>
         {this.props.children}
       </div>
     );
