7e314a2ac65a0a431e0073441c41d8afdedf6863
Joe Tanner <joe.j.tanner@gmail.com>
2017-05-24 15:19:10 -0600

    Pave the way for adding the "pill" type tabs
    * deprecate `useTabsInMobile` prop in favor of explicitly specifying `type=menu`
    * add a `type` prop, defaulting to 'standard'
    * extract MenuTabs and StandardTabs from Tabs
    * Tabs now chooses which style to render based on the `type` prop

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,27 +4,26 @@ const Radium = require('radium');
 
 const StyleConstants = require('../constants/Style');
 
-class Tabs extends React.Component {
+class StandardTabs extends React.Component {
   static propTypes = {
     activeTabStyles: PropTypes.object,
-    alignment: PropTypes.oneOf(['left', 'center']),
     brandColor: PropTypes.string,
     onTabSelect: PropTypes.func.isRequired,
     selectedTab: PropTypes.number,
-    showBottomBorder: PropTypes.bool,
-    tabs: PropTypes.array.isRequired,
-    useTabsInMobile: PropTypes.bool
+    tabs: PropTypes.array.isRequired
   };
 
   static defaultProps = {
-    alignment: 'left',
-    brandColor: StyleConstants.Colors.PRIMARY,
-    showBottomBorder: true
+    brandColor: StyleConstants.Colors.PRIMARY
   };
 
-  state = {
-    selectedTab: this.props.selectedTab || 0
-  };
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      selectedTab: this.props.selectedTab || 0
+    };
+  }
 
   componentWillMount () {
     if (this.props.hasOwnProperty('useTabsInMobile')) {
@@ -40,25 +39,22 @@ class Tabs extends React.Component {
     }
   }
 
-  _handleTabClick = (selectedTab) => {
+  handleTabSelect (selectedTab) {
     this.props.onTabSelect(selectedTab);
 
     this.setState({
       selectedTab
     });
-  };
+  }
 
   render () {
-    const styles = this.styles();
-    const componentStyles = Object.assign({}, styles.component, styles.tabsContainer, this.props.style);
-
     return (
-      <div style={componentStyles}>
+      <div>
         {this.props.tabs.map((tab, index) =>
           <Tab
             isActive={this.state.selectedTab === index}
             key={tab}
-            onClick={this._handleTabClick.bind(null, index)}
+            onClick={this.handleTabSelect.bind(this, index)}
             styles={{ activeTab: this.props.activeTabStyles }}
           >
             {tab}
@@ -67,24 +63,6 @@ class Tabs extends React.Component {
       </div>
     );
   }
-
-  styles = () => {
-    return {
-      // Block styles
-      component: {
-        display: 'block',
-        width: '100%'
-      },
-      tabsContainer: {
-        borderBottom: this.props.showBottomBorder ? '1px solid ' + StyleConstants.Colors.FOG : 'none',
-        boxSizing: 'border-box',
-        display: 'flex',
-        justifyContent: this.props.alignment === 'left' ? 'flex-start' : 'center',
-        overflowX: 'scroll',
-        width: '100%'
-      }
-    };
-  };
 }
 
 class TabWithoutRadium extends React.Component {
@@ -147,5 +125,47 @@ class TabWithoutRadium extends React.Component {
 }
 
 const Tab = Radium(TabWithoutRadium);
+
+const TabsTypes = {
+  standard: StandardTabs
+};
+
+const Tabs = ({
+  alignment = 'left',
+  showBottomBorder = true,
+  style,
+  type = 'standard',
+  useTabsInMobile,
+  ...props
+}) => {
+  if (typeof useTabsInMobile !== 'undefined') {
+    console.warn('The useTabsInMobile prop is deprecated and will be removed in a future release. Please use `type=\'menu\'`.');
+  }
+
+  const componentStyle = Object.assign({
+    borderBottom: showBottomBorder ? '1px solid ' + StyleConstants.Colors.FOG : 'none',
+    boxSizing: 'border-box',
+    display: 'flex',
+    justifyContent: alignment === 'left' ? 'flex-start' : 'center',
+    overflowX: 'scroll',
+    width: '100%'
+  }, style);
+  const TabsComponent = TabsTypes[type];
+
+  if (!TabsComponent) throw new Error(`Unknown Tabs type: ${type}`);
+
+  return (
+    <div style={componentStyle}>
+      <TabsComponent {...props} />
+    </div>
+  );
+};
+
+Tabs.propTypes = {
+  alignment: PropTypes.oneOf(['left', 'center']),
+  showBottomBorder: PropTypes.bool,
+  type: PropTypes.oneOf(Object.keys(TabsTypes)),
+  useTabsInMobile: PropTypes.bool
+};
 
 module.exports = Tabs;
