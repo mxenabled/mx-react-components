5b33405d1590868e5f25cb16a96947fe6e4d4d3d
Joe Tanner <joe.j.tanner@gmail.com>
2017-06-01 10:12:46 -0600

    Remove unused original adjustHexOpacity function

--- src/constants/Style.js
+++ src/constants/Style.js
@@ -95,14 +95,6 @@ module.exports = {
     XSMALL: 5
   },
 
-  adjustHexOpacity (color, opacity) {
-    const r = parseInt(color.slice(1, 3), 16);
-    const g = parseInt(color.slice(3, 5), 16);
-    const b = parseInt(color.slice(5, 7), 16);
-
-    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + opacity + ')';
-  },
-
   linearGradient (startColor, startOpacity = 0.8, endColor = startColor, endOpacity = 1) {
     return `linear-gradient(${this.adjustHexOpacity(startColor, startOpacity)}, ${this.adjustHexOpacity(endColor, endOpacity)})`;
   },
