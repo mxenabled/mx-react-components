6c908b54bde8c5b0367b9a6a3cf8496ef454b699
iheartrachie <rachelpipkin@gmail.com>
2017-06-07 14:53:06 -0600

    sets default input type

--- src/components/DisplayInput.js
+++ src/components/DisplayInput.js
@@ -28,7 +28,9 @@ class DisplayInput extends React.Component {
   };
 
   static defaultProps = {
-    elementProps: {},
+    elementProps: {
+      type: 'text'
+    },
     isFocused: false,
     primaryColor: StyleConstants.Colors.PRIMARY,
     valid: true
@@ -88,6 +90,7 @@ class DisplayInput extends React.Component {
                     id={this._inputId}
                     key='input'
                     style={styles.input}
+                    type='text'
                   />
                 </div>
               )}
