d21946c7d4bfa8e1adb6bf4f5d66ba141c6a2f12
Joe Tanner <joe.j.tanner@gmail.com>
2017-06-12 12:31:20 -0600

    Add theme to RadioButton

--- docs/components/RadioButtonDocs.js
+++ docs/components/RadioButtonDocs.js
@@ -1,6 +1,7 @@
 const React = require('react');
+const { Link } = require('react-router');
 
-const { RadioButton, Styles } = require('mx-react-components');
+const { RadioButton } = require('mx-react-components');
 
 const Markdown = require('components/Markdown');
 
@@ -54,14 +55,14 @@ class RadioButtonDocs extends React.Component {
         <h5>children <label>Boolean</label></h5>
         <p>Optional content included between opening and closing tags such as a label.</p>
 
-        <h5>color <label>String</label></h5>
-        <p>A string with a valid CSS color to use for the checked indicator. Defaults to <span style={{ color: Styles.Colors.PRIMARY }}>{Styles.Colors.PRIMARY}</span>. This can also be overridden with the activeButtonStyle prop.</p>
-
         <h5>onClick <label>Function</label></h5>
         <p>A callback function that will be triggered on component click.</p>
 
         <h5>style <label>Object</label></h5>
         <p>A style object that modifies the css styles of the 'div' element that wraps the component.</p>
+
+        <h5>theme <label>Object</label></h5>
+        <p>Customize the component&apos;s look. See <Link to='/components/theme'>Theme</Link> for more information.</p>
 
         <h3>Example</h3>
         <Markdown>
