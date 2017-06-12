71ae3c22e92611633867b7b8ff58c4347734d34c
Joe Tanner <joe.j.tanner@gmail.com>
2017-06-12 10:26:38 -0600

    Add theme to Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -3,18 +3,20 @@ const React = require('react');
 
 const Spin = require('./Spin');
 
-const StyleConstants = require('../constants/Style');
+const { themeShape } = require('../constants/App');
+
+const StyleUtils = require('../utils/Style');
 
 class Loader extends React.Component {
   static propTypes = {
     color: PropTypes.string,
     isLoading: PropTypes.bool,
     isRelative: PropTypes.bool,
-    isSmall: PropTypes.bool
+    isSmall: PropTypes.bool,
+    theme: themeShape
   };
 
   static defaultProps = {
-    color: StyleConstants.Colors.PRIMARY,
     isLoading: false,
     isRelative: false,
     isSmall: false,
@@ -22,49 +24,10 @@ class Loader extends React.Component {
   };
 
   render () {
-    if (this.props.isLoading) {
-      const styles = {
-        component: {
-          backgroundColor: 'rgba(255,255,255,0.9)',
-          bottom: 0,
-          color: '#999',
-          fontFamily: StyleConstants.FontFamily,
-          fontSize: '10px',
-          fontWeight: 600,
-          left: 0,
-          letterSpacing: '1px',
-          position: this.props.isRelative ? 'absolute' : 'fixed',
-          right: 0,
-          textAlign: 'center',
-          top: 0,
-          zIndex: 100
-        },
-        content: {
-          textAlign: 'center',
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          margin: 'auto',
-          width: this.props.isSmall ? '30px' : '50px',
-          height: this.props.isSmall ? '30px' : '50px'
-        },
-        circle: {
-          borderRadius: '100%',
-          width: this.props.isSmall ? '30px' : '50px',
-          height: this.props.isSmall ? '30px' : '50px',
-          borderTop: '3px solid ' + this.props.color,
-          borderRight: '3px solid transparent',
-          borderBottom: '3px solid transparent',
-          borderLeft: '3px solid transparent'
-        },
-        text: {
-          marginTop: '10px',
-          fontSize: '10px'
-        }
-      };
+    const theme = StyleUtils.mergeTheme(this.props.theme);
+    const styles = this.styles(theme);
 
+    if (this.props.isLoading) {
       return (
         <div className='mx-loader' style={styles.component}>
           <div className='mx-loader-content' style={styles.content}>
@@ -84,6 +47,52 @@ class Loader extends React.Component {
         <div />
       );
     }
+  }
+
+  styles = (theme) => {
+    const color = this.props.color || theme.Colors.PRIMARY;
+
+    return {
+      component: {
+        backgroundColor: 'rgba(255,255,255,0.9)',
+        bottom: 0,
+        color: '#999',
+        fontFamily: theme.Fonts.REGULAR,
+        fontSize: '10px',
+        fontWeight: 600,
+        left: 0,
+        letterSpacing: '1px',
+        position: this.props.isRelative ? 'absolute' : 'fixed',
+        right: 0,
+        textAlign: 'center',
+        top: 0,
+        zIndex: 100
+      },
+      content: {
+        textAlign: 'center',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        margin: 'auto',
+        width: this.props.isSmall ? '30px' : '50px',
+        height: this.props.isSmall ? '30px' : '50px'
+      },
+      circle: {
+        borderRadius: '100%',
+        width: this.props.isSmall ? '30px' : '50px',
+        height: this.props.isSmall ? '30px' : '50px',
+        borderTop: '3px solid ' + color,
+        borderRight: '3px solid transparent',
+        borderBottom: '3px solid transparent',
+        borderLeft: '3px solid transparent'
+      },
+      text: {
+        marginTop: '10px',
+        fontSize: '10px'
+      }
+    };
   }
 }
 
