c9ed368b5bfba42f15591aae4a2a41cbe6327d8d
Ryan Moore <ryan.moore@mx.com>
2017-07-18 16:40:19 -0600

    only showing calendar icon if not small device

--- src/components/DateRangePicker.js
+++ src/components/DateRangePicker.js
@@ -314,15 +314,18 @@ class DateRangePicker extends React.Component {
   render () {
     const styles = this.styles();
     const spans = this.spans();
+    const shouldShowCalendarIcon = StyleConstants.getWindowSize() !== 'small';
 
     return (
       <div style={styles.component}>
         <div onClick={this._toggleCalendar} style={styles.selectedDateWrapper}>
-          <Icon
-            size={20}
-            style={styles.selectedDateIcon}
-            type='calendar'
-          />
+          {shouldShowCalendarIcon ? (
+            <Icon
+              size={20}
+              style={styles.selectedDateIcon}
+              type='calendar'
+            />
+          ) : null}
           <div style={styles.selectedDateText}>
             {this.props.selectedStartDate && this.props.selectedEndDate ? (
               <div>
