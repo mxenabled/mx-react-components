3b7629922f2e8e29020b682bf244f5a8d251eb45
lukeschunk <luke.schunk@mx.com>
2017-11-02 15:22:59 -0600

    bumped version and update changelog

--- docs/components/Changelog.js
+++ docs/components/Changelog.js
@@ -8,6 +8,11 @@ class Changelog extends React.Component {
 
         <h2>MX React Components V 5.0</h2>
 
+        <h3>5.1.5</h3>
+        <ul>
+          <li>DateRangePicker Accessibility updates (<a href='https://github.com/mxenabled/mx-react-components/pull/680'>#680</a>)</li>
+        </ul>
+
         <h3>5.1.4</h3>
         <ul>
           <li>Setup Jest for testing.  (<a href='https://github.com/mxenabled/mx-react-components/pull/678'>#678</a>)</li>
