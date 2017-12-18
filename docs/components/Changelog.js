3586b313a6f8c5194d1a64e761d2ea568c0d8106
lukeschunk <luke.schunk@mx.com>
2017-12-18 15:36:07 -0700

    bump version to 5.1.10, changelog

--- docs/components/Changelog.js
+++ docs/components/Changelog.js
@@ -8,6 +8,11 @@ class Changelog extends React.Component {
 
         <h2>MX React Components V 5.0</h2>
 
+        <h3>5.1.10</h3>
+        <ul>
+          <li>Modal Accessibility Improvements (<a href='https://github.com/mxenabled/mx-react-components/pull/686'>#686</a>)</li>
+        </ul>
+
         <h3>5.1.9</h3>
         <ul>
           <li>Tweaks to SimpleSelect component to make accessibility easier (<a href='https://github.com/mxenabled/mx-react-components/pull/688'>#688</a>)</li>
