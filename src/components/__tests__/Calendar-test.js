1d499eae7b329e3ea8c37795b4708538e0b3333d
lukeschunk <luke.schunk@mx.com>
2018-01-17 15:55:55 -0700

    fixed test linting

--- src/components/__tests__/Calendar-test.js
+++ src/components/__tests__/Calendar-test.js
@@ -1,23 +1,23 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import { shallow } from 'enzyme';
 import moment from 'moment';
-import keycode from 'keycode';
+
 import { __calculateDayByKey } from '../../enhancers/calendar-enhancers';
 
 import Calendar from '../Calendar';
 
 describe('Calendar', () => {
-  let wrapper
+  let wrapper;
 
   beforeEach(() => {
     const theme = {
       Colors: {},
       Fonts: {},
       FontSizes: {},
-      Spacing: {},
-    }
+      Spacing: {}
+    };
 
-    const mockCallback = jest.fn()
+    const mockCallback = jest.fn();
 
     wrapper = shallow(
       <Calendar
@@ -25,40 +25,38 @@ describe('Calendar', () => {
         selectedDate={moment().unix()}
         theme={theme}
       />
-    )
-  })
+    );
+  });
 
 
   describe('rendering', () => {
     it('should mount and render 35 calendar days', () => {
-      expect(wrapper.find('.calendar-day').length).toBe(35)
-    })
+      expect(wrapper.find('.calendar-day').length).toBe(35);
+    });
 
     it('should only render one focused day', () => {
-      expect(wrapper.find('#focused-day').length).toBe(1)
-    })
-  })
+      expect(wrapper.find('#focused-day').length).toBe(1);
+    });
+  });
 
 
   describe('__calculateDayByKey', () => {
     const focusedDay = moment().unix();
 
     it('should return correct day when right key is pressed', () => {
-      expect(__calculateDayByKey('right', focusedDay)).toEqual(moment.unix(focusedDay).add(1, 'days').startOf('day'))
-    })
+      expect(__calculateDayByKey('right', focusedDay)).toEqual(moment.unix(focusedDay).add(1, 'days').startOf('day'));
+    });
 
     it('should return correct day when left key is pressed', () => {
-      expect(__calculateDayByKey('left', focusedDay)).toEqual(moment.unix(focusedDay).subtract(1, 'days').startOf('day'))
-    })
+      expect(__calculateDayByKey('left', focusedDay)).toEqual(moment.unix(focusedDay).subtract(1, 'days').startOf('day'));
+    });
 
     it('should return correct day when up key is pressed', () => {
-      expect(__calculateDayByKey('up', focusedDay)).toEqual(moment.unix(focusedDay).subtract(7, 'days').startOf('day'))
-    })
+      expect(__calculateDayByKey('up', focusedDay)).toEqual(moment.unix(focusedDay).subtract(7, 'days').startOf('day'));
+    });
 
     it('should return correct day when down key is pressed', () => {
-      expect(__calculateDayByKey('down', focusedDay)).toEqual(moment.unix(focusedDay).add(7, 'days').startOf('day'))
-    })
-
-  })
-
+      expect(__calculateDayByKey('down', focusedDay)).toEqual(moment.unix(focusedDay).add(7, 'days').startOf('day'));
+    });
+  });
 });
