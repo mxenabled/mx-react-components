e01a068721600ea590f0810739fd9996b5ce16eb
cerinman <tommy.dugger78@gmail.com>
2018-03-26 13:54:30 -0600

    Adds test for MXFocusTrap

---
+++ src/components/__tests__/MXFocusTrap-test.js
@@ -0,0 +1,26 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { mount } from 'enzyme';
+
+const { MXFocusTrap, WrappedMXFocusTrap } = require('../MXFocusTrap');
+
+describe('MXFocusTrap', () => {
+  it('should pause and un-pause parent trap when child trap mounts and unmounts', () => {
+    expect.assertions(2);
+
+    /**
+     * WrappedMXFocusTrap renders a MXFocusTrap within a MXFocusTrap
+     * and provides a way to unmount the child trap by setting
+     * state on the wrapper.
+     **/
+    const wrapper = mount(<WrappedMXFocusTrap />);
+    const parentTrap = wrapper.find(MXFocusTrap).first().instance();
+
+    // Parent should be un-paused when child is unmounted.
+    wrapper.setState({ renderChildTrap: false });
+    expect(parentTrap.state.paused).toEqual(false);
+
+    // Parent should be paused when child is unmounted.
+    wrapper.setState({ renderChildTrap: true });
+    expect(parentTrap.state.paused).toEqual(true);
+  });
+});
