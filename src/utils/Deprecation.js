dbfdd13dcb65e27d50c152382f1cb4dd1692017d
cerinman <tommy.dugger78@gmail.com>
2018-03-31 10:49:21 -0600

    Adds deprecation util for handleResetClick prop

--- src/utils/Deprecation.js
+++ src/utils/Deprecation.js
@@ -8,5 +8,10 @@ module.exports = {
     if (props[iconPropName]) {
       console.warn(`The ${iconPropName} prop is deprecated and will be removed in a future release. Use prefix and suffix props instead. http://mxenabled.github.io/mx-react-components/#/components/simple-input`);
     }
+  },
+  deprecateSimpleInputHandleResetClick (props, iconPropName = 'handleResetClick') {
+    if (props[iconPropName]) {
+      console.warn(`The ${iconPropName} prop is deprecated and will be removed in a future release. Use suffix prop by passing an element with a click handler instead. http://mxenabled.github.io/mx-react-components/#/components/simple-input`);
+    }
   }
 };
