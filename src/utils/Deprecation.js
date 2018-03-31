adf1ed41809d6088739a763fe43a9779daa1f16d
cerinman <tommy.dugger78@gmail.com>
2018-03-31 10:36:58 -0600

    Adds deprecation util for icon and rightIcon props of the SimpleInput component

--- src/utils/Deprecation.js
+++ src/utils/Deprecation.js
@@ -3,5 +3,10 @@ module.exports = {
     if (props[primaryColorPropName]) {
       console.warn(`${primaryColorPropName} is deprecated and will be removed in a future release. Use theme instead.`);
     }
+  },
+  deprecateSimpleInputIcon (props, iconPropName = 'icon') {
+    if (props[iconPropName]) {
+      console.warn(`The ${iconPropName} prop is deprecated and will be removed in a future release. Use prefix and suffix props instead. http://mxenabled.github.io/mx-react-components/#/components/simple-input`);
+    }
   }
 };
