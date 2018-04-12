115599a8712ac26d16435932ce78656f9fd268df
Joe Tanner <joe.j.tanner@gmail.com>
2018-04-12 10:42:14 -0600

    Update tests since SimpleInput is now wrapped by withTheme()

--- src/components/__tests__/SimpleInput-test.js
+++ src/components/__tests__/SimpleInput-test.js
@@ -1,38 +1,42 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 const Icon = require('../Icon');
 const SimpleInput = require('../SimpleInput');
 
 describe('SimpleInput', () => {
   it('should render a input', () => {
-    const wrapper = shallow(<SimpleInput />);
+    const wrapper = mount(<SimpleInput />);
 
     expect(wrapper.find('input')).toHaveLength(1);
   });
 
   it('should render a prefix to the left of the input if prop provided', () => {
-    const wrapper = shallow(<SimpleInput prefix={(<span>Prefix</span>)} />);
+    const wrapper = mount(<SimpleInput prefix={(<span>Prefix</span>)} />);
+    const children = wrapper.find('div').first().children();
 
-    expect(wrapper.children().first().html()).toEqual('<span>Prefix</span>');
+    expect(children.first().html()).toEqual('<span>Prefix</span>');
   });
 
   it('should render a suffix to the right of the input if prop provided', () => {
-    const wrapper = shallow(<SimpleInput suffix={(<span>Suffix</span>)} />);
+    const wrapper = mount(<SimpleInput suffix={(<span>Suffix</span>)} />);
+    const children = wrapper.find('div').first().children();
 
-    expect(wrapper.children().last().html()).toEqual('<span>Suffix</span>');
+    expect(children.last().html()).toEqual('<span>Suffix</span>');
   });
 
   it('should render a icon to the left of the input if icon prop provided', () => {
-    const wrapper = shallow(<SimpleInput icon='cash' />);
+    const wrapper = mount(<SimpleInput icon='cash' />);
+    const children = wrapper.find('div').first().children();
 
-    expect(wrapper.children().first().type()).toEqual(Icon);
+    expect(children.first().type()).toEqual(Icon);
   });
 
   it('should render a icon to the right of the input if rightIcon prop provided', () => {
-    const wrapper = shallow(<SimpleInput rightIcon='cash' />);
+    const wrapper = mount(<SimpleInput rightIcon='cash' />);
+    const children = wrapper.find('div').first().children();
 
-    expect(wrapper.children().last().type()).toEqual(Icon);
+    expect(children.last().type()).toEqual(Icon);
   });
 
   it('should provide a ref to the input via the elementRef prop', () => {
@@ -40,6 +44,6 @@ describe('SimpleInput', () => {
 
     const wrapper = mount(<SimpleInput elementRef={ref => inputRef = ref} />);
 
-    expect(inputRef).toEqual(wrapper.instance().elementRef);
+    expect(inputRef).toEqual(wrapper.find('input').instance());
   });
 });
