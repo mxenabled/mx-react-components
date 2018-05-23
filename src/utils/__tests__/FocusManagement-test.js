c95e424946b56eda0364bef5726bd5632ee1949e
cerinman <tommy.dugger78@gmail.com>
2018-05-23 10:00:13 -0600

    Makes test for getFocusableNodesInElement more robust

--- src/utils/__tests__/FocusManagement-test.js
+++ src/utils/__tests__/FocusManagement-test.js
@@ -5,30 +5,56 @@ import React from 'react';
 describe('FocusManagement utils', () => {
   let page;
   let child;
+  let button;
+  let link;
+  let focusableDiv;
+  let select;
+  let textArea;
+  let input;
 
   beforeEach(() => {
+    button = mount(<button>Child Button</button>);
+    link = mount(<a href='url'>link in content</a>);
+    focusableDiv = mount(<div tabIndex={0}>Focusable Div</div>);
+    select = mount(<select />);
+    textArea = mount(<textarea />);
+    input = mount(<input />);
+
     child = mount(
       <div>
         Child content
-        <button>Child Button</button>
+        {button}
       </div>
     );
 
     page = mount(
       <div>
-        <button>click me</button>
-        <div>Content <a href='url'>link in content</a></div>
-        <div tabIndex={0}>Focusable Div</div>
+        <div>Content {link}</div>
+        <div aria-hidden={true} tabIndex={0}>Aria hidden with tab index</div>
+        <div tabIndex={-1}>Aria visible but tab index of -1</div>
+        {focusableDiv}
         {child}
+        {select}
+        {textArea}
+        {input}
       </div>
     );
   });
 
   describe('getFocusableNodesInElement', () => {
-    it('should find all focusable nodes', () => {
+    it('should find all focusable nodes that are NOT aria-hidden or have tabIndex 0f -1', () => {
       const focusableNodes = FocusManagement.getFocusableNodesInElement(page.instance());
+      const expectedArrayOfNodes = [
+        link.instance(),
+        focusableDiv.instance(),
+        button.instance(),
+        select.instance(),
+        textArea.instance(),
+        input.instance()
+      ];
 
-      expect(focusableNodes).toHaveLength(4);
+      expect(focusableNodes).toHaveLength(6);
+      expect(focusableNodes).toEqual(expectedArrayOfNodes);
     });
   });
 });
