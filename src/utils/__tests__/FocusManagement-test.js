83194de84f8abea506fbf2324b180bf7cb17cb58
cerinman <tommy.dugger78@gmail.com>
2018-05-23 09:37:30 -0600

    Adds test to check focusable items found with getFocusableNodesInElement

--- src/utils/__tests__/FocusManagement-test.js
+++ src/utils/__tests__/FocusManagement-test.js
@@ -1,3 +1,34 @@
 import FocusManagement from '../FocusManagement';
+import { mount } from 'enzyme';
+import React from 'react';
 
-describe('FocusManagement utils', () => {});
+describe('FocusManagement utils', () => {
+  let page;
+  let child;
+
+  beforeEach(() => {
+    child = mount(
+      <div>
+        Child content
+        <button>Child Button</button>
+      </div>
+    );
+
+    page = mount(
+      <div>
+        <button>click me</button>
+        <div>Content <a href='url'>link in content</a></div>
+        <div tabIndex={0}>Focusable Div</div>
+        {child}
+      </div>
+    );
+  });
+
+  describe('getFocusableNodesInElement', () => {
+    it('should find all focusable nodes', () => {
+      const focusableNodes = FocusManagement.getFocusableNodesInElement(page.instance());
+
+      expect(focusableNodes).toHaveLength(4);
+    });
+  });
+});
