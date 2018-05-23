c75dc869170fdad843ce5d0edef8638a5c54f7e5
cerinman <tommy.dugger78@gmail.com>
2018-05-23 15:23:02 -0600

    Adds base set of tests for RestrictFocusToChildren

---
+++ src/components/__tests__/RestrictFocusToChildren-test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+const RestrictFocusToChildren = require('../RestrictFocusToChildren');
+
+class TestComponent extends React.Component {
+  state = {
+    showContent: false
+  }
+
+  render () {
+    return (
+      <div>
+        <button
+          id='open-button'
+          onClick={e => {
+            //Enzyme simulate click was not focusing the button
+            e.target.focus();
+            this.setState({ showContent: true });
+          }}
+        >
+          Open Content
+        </button>
+        {this.state.showContent ? (
+          <RestrictFocusToChildren>
+            <div>
+              <button id='close-button' onClick={() => this.setState({ showContent: false })}>
+                Close Content
+              </button>
+            </div>
+          </RestrictFocusToChildren>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+describe('RestrictFocusToChildren', () => {
+  let openButton;
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(<TestComponent />, { attachTo: window.document });
+    openButton = wrapper.find('#open-button').first();
+
+    openButton.simulate('click');
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it('should remember the last focused node', () => {
+    expect(wrapper.find(RestrictFocusToChildren).first().instance().previousFocusedNode)
+      .toEqual(openButton.instance());
+  });
+
+  it('should hide content in parent that is focusable when child is rendered', () => {
+    const buttonHTML = openButton.html();
+
+    expect(buttonHTML).toContain('tabindex="-1"');
+    expect(buttonHTML).toContain('aria-hidden="true"');
+  });
+
+  it('should unhide content in parent that is focusable when child is removed', () => {
+    const closeButton = wrapper.find('#close-button').first();
+
+    closeButton.simulate('click');
+
+    const openButtonHTML = openButton.html();
+
+    expect(openButtonHTML).not.toContain('tabindex');
+    expect(openButtonHTML).not.toContain('aria-hidden');
+  });
+});
