d5ec00f59d9969be4a901a4c3bbbbbbdc7cc760c
cerinman <tommy.dugger78@gmail.com>
2018-07-27 15:04:46 -0600

    Update jest setup to mock window and document with jsdom

--- jest-setup.js
+++ jest-setup.js
@@ -1,4 +1,11 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { JSDOM } from 'jsdom';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+global.window = window;
+global.document = window.document;
