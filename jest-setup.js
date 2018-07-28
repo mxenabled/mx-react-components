341b20685ac01ef798a20eceab721294f7500c14
cerinman <tommy.dugger78@gmail.com>
2018-07-28 15:58:05 -0600

    remove jsdom dep and usage

--- jest-setup.js
+++ jest-setup.js
@@ -1,10 +1,4 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { JSDOM } from 'jsdom';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-const jsdom = new JSDOM('<!doctype html><html><body><div></div></body></html>');
-
-global.window = jsdom.window;
-global.document = jsdom.window.document;
