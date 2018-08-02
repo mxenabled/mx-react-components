a8fb57bc598c2587f7c80cb8bbb044b027252849
cerinman <tommy.dugger78@gmail.com>
2018-08-02 13:37:45 -0600

    Revert changelog for 5.2.23

--- docs/components/Changelog.js
+++ docs/components/Changelog.js
@@ -7,11 +7,6 @@ class Changelog extends React.Component {
         <h1>Change Log</h1>
 
         <h2>MX React Components V 5.0</h2>
-        <h3>5.2.23</h3>
-        <ul>
-          <li>A11y fix for Modal and Drawer components(<a href='https://github.com/mxenabled/mx-react-components/pull/767'>#767</a>)</li>
-        </ul>
-
         <h3>5.2.22</h3>
         <ul>
           <li>Search Input Typo:(<a href='https://github.com/mxenabled/mx-react-components/pull/764'>#766</a>)</li>
