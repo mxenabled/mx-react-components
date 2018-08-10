335dd0e94bef86e0255ad9448d06b8e467406181
cerinman <tommy.dugger78@gmail.com>
2018-08-10 08:47:48 -0600

    Tweak docs some more

--- docs/components/IconDocs.js
+++ docs/components/IconDocs.js
@@ -2,13 +2,15 @@
 /* eslint-disable react/jsx-indent */
 const React = require('react');
 
-const { Button, Icon, Styles, AppConstants } = require('mx-react-components');
+const { Icon, SimpleInput, Styles, AppConstants } = require('mx-react-components');
 
 const Markdown = require('components/Markdown');
 
 class IconDocs extends React.Component {
   state = {
-    fillBackground: false
+    backgroundFill: 'transparent',
+    iconFill: Styles.Colors.GRAY_700,
+    iconSize: 40
   }
 
   render () {
@@ -25,10 +27,10 @@ class IconDocs extends React.Component {
             return (
               <div key={icon.value} style={{ width: 100, height: 100, textAlign: 'center' }}>
                 <Icon
-                  backgroundFillColor={this.state.fillBackground ? Styles.Colors.LIGHT_WARNING : 'transparent'}
+                  backgroundFillColor={this.state.backgroundFill}
                   key={icon.value}
-                  size={40}
-                  style={{ fill: this.state.fillIcon ? Styles.Colors.PRIMARY : Styles.Colors.GRAY_900 }}
+                  size={this.state.iconSize}
+                  style={{ fill: this.state.iconFill }}
                   type={icon.value}
                 />
                 <div style={{ color: Styles.Colors.GRAY_500, fontSize: Styles.FontSizes.SMALL, marginTop: 5 }}>({icon.value})</div>
@@ -40,16 +42,81 @@ class IconDocs extends React.Component {
         <h3>Usage</h3>
         <h5>backgroundFillColor <label>String</label></h5>
         <p>Default: 'transparent'</p>
-        <p>Some icons have extra svg path data to provide a background fill incase you want parts of them to not be see through. You can set this prop to any valid color or hexadecimal value to change the the background fill on supported icons. Click the button below to see the supported icons toggle their background fill color.</p>
-        <p><Button onClick={() => (this.setState(state => ({ fillBackground: !state.fillBackground })))}>Toggle Background Fill Color</Button></p>
+        <p>Some icons have extra svg path data to provide a background fill incase you want parts of them to not be see through. You can set this prop to any valid html color or hexadecimal color value to change the the background fill on supported icons. Use the input below to update the background fill color to see the supported icons update.</p>
+        <p>
+          <SimpleInput
+            elementProps={{
+              defaultValue: this.state.backgroundFill,
+              onChange: e => (this.setState({ backgroundFill: e.target.value }))
+            }}
+            prefix={(
+              <span style={{ marginRight: 10 }}>Background Fill</span>
+            )}
+            styles={{
+              wrapper: {
+                width: '50%'
+              }
+            }}
+            suffix={(
+              <div style={{
+                backgroundColor: this.state.backgroundFill,
+                border: `1px solid ${Styles.Colors.GRAY_700}`,
+                height: 20,
+                width: 20
+              }}
+              />
+            )}
+            type='text'
+          />
+        </p>
 
         <h5>size <label>Number, String</label></h5>
-        <p>A single number representing the width and height of the icon in pixels.</p>
-
+        <p>A single number representing the width and height of the icon in pixels. Change the icon's size by using the number input below.</p>
+        <p>
+          <SimpleInput
+            elementProps={{
+              defaultValue: this.state.iconSize,
+              onChange: e => (this.setState({ iconSize: e.target.value }))
+            }}
+            prefix={(
+              <span style={{ marginRight: 10 }}>Icon Size</span>
+            )}
+            styles={{
+              wrapper: {
+                width: '50%'
+              }
+            }}
+            type='number'
+          />
+        </p>
         <h5>style <label>object</label></h5>
-        <p>A style object applied to the main svg element of the icon. You can use this to over ride styling or change the fill color of the icon. Click the button below to over ride the style and change the icon fill color to blue.</p>
-        <p><Button onClick={() => (this.setState(state => ({ fillIcon: !state.fillIcon })))}>Toggle Main SVG Fill Color</Button></p>
-
+        <p>A style object applied to the main svg element of the icon. You can use this to over ride styling or change the fill color of the icon. Use the input below to over ride the style and change the icon fill to a differnt color.</p>
+        <p>
+          <SimpleInput
+            elementProps={{
+              defaultValue: this.state.iconFill,
+              onChange: e => (this.setState({ iconFill: e.target.value }))
+            }}
+            prefix={(
+              <span style={{ marginRight: 10 }}>Icon Fill</span>
+            )}
+            styles={{
+              wrapper: {
+                width: '50%'
+              }
+            }}
+            suffix={(
+              <div style={{
+                backgroundColor: this.state.iconFill,
+                border: `1px solid ${Styles.Colors.GRAY_700}`,
+                height: 20,
+                width: 20
+              }}
+              />
+            )}
+            type='text'
+          />
+        </p>
         <h5>type <label>String</label></h5>
         <p>The name of the icon to be displayed. See above for available options.</p>
 
