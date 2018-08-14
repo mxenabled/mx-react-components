411db5560e97ed31f5c65dcadefea2f25758dd62
cerinman <tommy.dugger78@gmail.com>
2018-08-14 15:59:15 -0600

    Removes deprecatePrimaryColor utils function

--- src/utils/Deprecation.js
+++ src/utils/Deprecation.js
@@ -1,9 +1,4 @@
 module.exports = {
-  deprecatePrimaryColor (props, primaryColorPropName = 'primaryColor') {
-    if (props[primaryColorPropName]) {
-      console.warn(`${primaryColorPropName} is deprecated and will be removed in a future release. Use theme instead.`);
-    }
-  },
   deprecateProp (props, deprecatedPropName, replacementPropName, componentDocsURL) {
     if (props[deprecatedPropName]) {
       console.warn(`The ${deprecatedPropName} prop is deprecated and will be removed in a future release. Use ${replacementPropName} instead. http://mxenabled.github.io/mx-react-components/#/components/${componentDocsURL}`);
