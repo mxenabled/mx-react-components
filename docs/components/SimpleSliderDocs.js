abf4e71a731c54df3c6083f801f357a7271d3649
Mara BD <mara.dean@mx.com>
2018-08-21 17:31:41 -0600

    updated simple slider

--- docs/components/SimpleSliderDocs.js
+++ docs/components/SimpleSliderDocs.js
@@ -27,14 +27,20 @@ class SimpleSliderDocs extends React.Component {
         <SimpleSlider onPercentChange={this._handleSliderChange} percent={this.state.percent} />
 
         <h3>Usage</h3>
+        <h5>disabled <label>Boolean</label></h5>
+        <p>When true, the slider is disabled. This defaults to false.</p>
+
+        <h5>elementRef <label>Function</label></h5>
+        <p>A callback function for getting the ref of the wrapping div of the SimpleSlider component.</p>
+
+        <h5>onPercentChange <label>Function</label> <em>Required</em></h5>
+        <p>When the slider is moved, your callback function is called and the current percent is passed as a param. The current percent is a number between 0 and 1.</p>
+
         <h5>percent <label>Number</label> <em>Required</em></h5>
         <p>This prop moves the slider to the assigned postion. It is passed in as a number between 0 and 1. So 0.75 would put the slider at 75% of the total distance.</p>
 
-        <h5>disabled <label>bool</label></h5>
-        <p>When true, the slider is disabled. This defaults to false.</p>
-
-        <h5>onPercentChange <label>func</label> <em>Required</em></h5>
-        <p>When the slider is moved, your callback function is called and the current percent is passed as a param. The current percent is a number between 0 and 1.</p>
+        <h5>styles <label>Object</label></h5>
+        <p>A nested styles object to override/append any of the styled elements.</p>
 
         <h5>theme <label>Object</label></h5>
         <p>Customize the component&apos;s look. See <Link to='/components/theme'>Theme</Link> for more information.</p>
@@ -54,4 +60,3 @@ class SimpleSliderDocs extends React.Component {
 }
 
 module.exports = SimpleSliderDocs;
-
