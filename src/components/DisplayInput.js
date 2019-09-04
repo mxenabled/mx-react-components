14724e86ebc98ec1cf8eaa3c3ea1eedf82282869
Jameson Brown <jamesonbrown@neolivity.com>
2019-09-04 14:50:52 -0600

    Added ability to style parent Container

--- src/components/DisplayInput.js
+++ src/components/DisplayInput.js
@@ -85,7 +85,7 @@ class DisplayInput extends React.Component {
     const styles = this.styles(theme, isLargeOrMediumWindowSize);
 
     return (
-      <Container className='mx-display-input'>
+      <Container styles={styles.container} className='mx-display-input'>
         <div style={Object.assign({}, styles.wrapper, this.props.isFocused ? styles.wrapperFocus : {})}>
           <Row>
             {this.props.label ? (
