98cbe2607fc236e11fbf094f2368bc5cf428b04c
cerinman <tommy.dugger78@gmail.com>
2019-09-19 15:34:33 -0600

    Correct change log after version change

--- docs/components/Changelog.js
+++ docs/components/Changelog.js
@@ -6,12 +6,14 @@ class Changelog extends React.Component {
       <div>
         <h1>Change Log</h1>
 
-        <h2>MX React Components V 6.5.0</h2>
+        <h2>MX React Components V 6.6.0</h2>
 
-        <h3>6.5.2</h3>
+        <h3>6.6.0</h3>
         <ul>
           <li>DateRangePicker - Render Props for custom trigger element. (<a href='https://github.com/mxenabled/mx-react-components/issues/841'>#841</a>)</li>
         </ul>
+
+        <h2>MX React Components V 6.5.0</h2>
 
         <h3>6.5.1</h3>
         <ul>
