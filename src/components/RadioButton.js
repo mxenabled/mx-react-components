9b50d1ac8fb2bb43487afe3d53864ee0331deb70
Tayte Stokes <tayte.stokes@mx.com>
2020-09-14 10:46:58 -0600

    implements radium to give radio button correct focus outline

--- src/components/RadioButton.js
+++ src/components/RadioButton.js
@@ -1,10 +1,11 @@
-const PropTypes = require('prop-types');
-const React = require('react');
+const PropTypes = require('prop-types')
+const React = require('react')
+const Radium = require('radium')
 
-import { withTheme } from './Theme';
-const { themeShape } = require('../constants/App');
+import { withTheme } from './Theme'
+const { themeShape } = require('../constants/App')
 
-const StyleUtils = require('../utils/Style');
+const StyleUtils = require('../utils/Style')
 
 class RadioButton extends React.Component {
   static propTypes = {
@@ -15,65 +16,79 @@ class RadioButton extends React.Component {
     elementRef: PropTypes.func,
     onClick: PropTypes.func,
     style: PropTypes.object,
-    theme: themeShape
-  };
+    theme: themeShape,
+  }
 
   static defaultProps = {
-    onClick: () => { }
-  };
+    onClick: () => {},
+  }
 
   render() {
-    const theme = StyleUtils.mergeTheme(this.props.theme);
-    const styles = this.styles(theme);
+    const theme = StyleUtils.mergeTheme(this.props.theme)
+    const styles = this.styles(theme)
 
     return (
       <button
         aria-checked={this.props.checked}
-        className='mx-radio-button-item'
+        className="mx-radio-button-item"
         onClick={this.props.onClick}
         ref={this.props.elementRef}
         role="radio"
         style={styles.component}
         {...this.props.elementProps}
       >
-        <div className='mx-radio-button' style={styles.radioButton}>
+        <div className="mx-radio-button" style={styles.radioButton}>
           {this.props.checked ? <div style={styles.radioButtonActive} /> : null}
         </div>
         <div style={styles.children}>{this.props.children}</div>
       </button>
-    );
+    )
   }
 
   styles = (theme) => {
     return {
-      component: Object.assign({}, {
-        cursor: 'pointer',
-        display: 'flex',
-        alignItems: 'center',
-        backgroundColor: 'transparent',
-        outline: 'none',
-        border: 'none',
-        fontSize: theme.FontSizes.MEDIUM,
-      }, this.props.style),
-      radioButton: Object.assign({}, {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 15,
-        height: 15,
-        marginRight: 5,
-        border: '1px solid ' + theme.Colors.GRAY_300,
-        borderRadius: '100%',
-        backgroundColor: theme.Colors.WHITE
-      }, this.props.buttonStyle),
-      radioButtonActive: Object.assign({}, {
-        width: '60%',
-        height: '60%',
-        borderRadius: '100%',
-        backgroundColor: theme.Colors.PRIMARY
-      }, this.props.activeButtonStyle)
-    };
-  };
+      component: Object.assign(
+        {},
+        {
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          backgroundColor: 'transparent',
+          border: 'none',
+          fontSize: theme.FontSizes.MEDIUM,
+          ':focus': {
+            outline: 'dotted thin #333',
+          },
+        },
+        this.props.style,
+      ),
+      radioButton: Object.assign(
+        {},
+        {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          width: 15,
+          height: 15,
+          marginRight: 5,
+          border: '1px solid ' + theme.Colors.GRAY_300,
+          borderRadius: '100%',
+          backgroundColor: theme.Colors.WHITE,
+        },
+        this.props.buttonStyle,
+      ),
+      radioButtonActive: Object.assign(
+        {},
+        {
+          width: '60%',
+          height: '60%',
+          borderRadius: '100%',
+          backgroundColor: theme.Colors.PRIMARY,
+        },
+        this.props.activeButtonStyle,
+      ),
+    }
+  }
 }
 
-module.exports = withTheme(RadioButton);
+module.exports = withTheme(Radium(RadioButton))
