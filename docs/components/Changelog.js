1c728108b3b0b70600d4480113dc892675a75867
Peter Garrow <peter.garrow@mx.com>
2021-04-12 14:59:51 -0600

    Docs Version: 8.2.10

--- docs/components/Changelog.js
+++ docs/components/Changelog.js
@@ -7,6 +7,11 @@ class Changelog extends React.Component {
         <h1>Change Log</h1>
 
         <h2>MX React Components V 8.0.0</h2>
+        <h3>8.2.10</h3>
+        <ul>
+          <li>Resets selectedDefaultRange to an empty string when dates in the calendar are selected.</li>
+        </ul>
+
         <h3>8.2.9</h3>
         <ul>
           <li>Adds 3rd param to onDateRangeSelect to pass the selectedDefaultRange in the callback.</li>
